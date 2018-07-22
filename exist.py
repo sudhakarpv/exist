9edf00c0f747db246196aa5360feec84abb4d5e5
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-22 18:26:35 +0530

    Update and rename README.md to exist.py

---
+++ exist.py
@@ -0,0 +1,13 @@
+# exist
+def main():
+    pass
+    inphw=input().split()
+    inp=input().split()
+    lis1=list(inphw)
+    lis2=list(inp)
+    if (lis1[1] in lis2):
+        print("yes")
+    else:
+        print("no")
+if __name__ == '__main__':
+    main()
